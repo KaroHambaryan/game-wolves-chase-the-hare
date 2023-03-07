a4d476153249876ba407aa3052133e53544fbbfd
Karo Hambaryan <karohambaryan@gmail.com>
2023-03-07 11:44:15 +0400

    add BoardSizeSelecton component

---
+++ src/Components/BoardSizeSelecton/BoardSizeSelecton.js
@@ -0,0 +1,7 @@
+import boardSizeSelectonStyle from './BoardSizeSelecton.module.css'
+
+const BoardSizeSelecton = () => {
+	return <div className={boardSizeSelectonStyle}></div>
+}
+
+export default BoardSizeSelecton;

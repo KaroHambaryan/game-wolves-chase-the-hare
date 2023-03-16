a185da407fe757fe3b06091cc93337520f253e3d
Karo Hambaryan <karohambaryan@gmail.com>
2023-03-16 13:50:10 +0400

    next walk for wolf

--- src/features/wolves/wolvesSlice.js
+++ src/features/wolves/wolvesSlice.js
@@ -3,6 +3,7 @@ export function changeWolvesCoordinates(state = {}, action) {
 		const [wolf1, wolf2, wolf3, wolf4, wolf5] = action.payload.arrayWithRandomCordinates;
 		const size = action.payload.boardSize;
 		if (size === 5) {
+			
 			return {
 				wolf1,
 				wolf2,
@@ -21,6 +22,12 @@ function nextWalk() {
 	const start = {};
 	const end = {};
 	const closed_list = [];
+	const boardSize = 0;
+	const matrix = createMatrix(end, boardSize);
+	const newMatrix = removeМatchWithMatrix(closed_list, matrix);
+	const neighborsArray = createStartNeighbor(start, boardSize);
+	const start_neighbor = searchStartNeighbor(newMatrix, neighborsArray);
+	const min_coords = createCoordinate(start_neighbor);
 
 	function createMatrix(end, boardSize) {
 		const matrix = [];
@@ -28,8 +35,8 @@ function nextWalk() {
 		for (let i = 0; i < boardSize; i++) {
 			const row = [];
 			for (let j = 0; j < boardSize; j++) {
-				let gx = Math.abs(end.x - i);
-				let gy = Math.abs(end.y - j);
+				let gx = Math.pow(Math.abs(end.x - i),2);
+				let gy = Math.pow(Math.abs(end.y - j),2);
 				let g = gx + gy
 				let x = i;
 				let y = j;
@@ -47,17 +54,61 @@ function nextWalk() {
 		for (let i = 0; i < newMatrix.length; i++) {
 			let ifInArray = closed_list.some((elem) => elem.x === newMatrix[i].x && elem.y === newMatrix[i].y)
 			if (ifInArray) {
-				newMatrix.splice(i,1)
+				newMatrix.splice(i, 1)
 			}
 		}
 		return newMatrix;
 	}
 
-	function searchStartNeighbor(newMatrix, start){
-
+	function createStartNeighbor(start, boardSize) {
+		const neighborsArray = [];
+	
+		if (start.y > 0) {
+			let up_coordinates = { x: start.x, y: start.y - 1 };
+			neighborsArray.push(up_coordinates);
+		}
+	
+		if (start.y < boardSize - 1) {
+			let down_coordinates = { x: start.x, y: start.y + 1 };
+			neighborsArray.push(down_coordinates);
+		}
+	
+		if (start.x < boardSize - 1) {
+			let right_coordinates = { x: start.x + 1, y: start.y };
+			neighborsArray.push(right_coordinates);
+		}
+	
+		if (start.x > 0) {
+			let left_coordinates = { x: start.x - 1, y: start.y };
+			neighborsArray.push(left_coordinates);
+		}
+	
+		return neighborsArray;
 	}
 
+	function searchStartNeighbor(newMatrix, neighborsArray) {
+		const start_neighbor = [];
+		for (let i = 0; i < newMatrix.length; i++) {
+			let ifInArray = neighborsArray.some((elem) => elem.x === newMatrix[i].x && elem.y === newMatrix[i].y)
+			if (ifInArray) {
+				start_neighbor.push(newMatrix[i]);
+			}
+		}
+		return start_neighbor;
+	}
 
+	function createCoordinate(start_neighbor) {
+		let min = start_neighbor[0];
+		for (let i = 0; i < start_neighbor.length; i++) {
+			if (start_neighbor[i].g < min.g) {
+				min = start_neighbor[i];
+			}
+			
+		}
+		return min;
+	}
+
+return min_coords
 
 }
 
